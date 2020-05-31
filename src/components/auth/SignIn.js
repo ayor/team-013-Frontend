ad6e8c067c563de2f91551d147a25bbcc9a3ba93
David Daniel <danielimodoye@gmail.com>
2020-05-31 07:55:03 +0100

    ui management redesign

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Navbar from '../layout/Navbar';
 import Footer from '../footer/Footer';
+import Modal from '../modal/modal';
 
 
 class SignIn extends Component {
@@ -9,10 +10,13 @@ class SignIn extends Component {
     super(props);
     this.state = {
       loginKey: '',
-      password: ''
+      password: '',
+      show: false 
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
   }
 
   handleChange(event) {
@@ -22,7 +26,17 @@ class SignIn extends Component {
     });
   }
 
-  handleSubmit(event) {
+  showModal() {
+    this.setState({ show: true });
+  };
+
+  hideModal() {
+    (this.state.message=='succesfully logged in')?this.props.history.push('/dashboard'):this.props.history.push('/signin');
+    this.setState({ show: false });
+      window.location.reload();
+  };
+
+   handleSubmit(event) {
     event.preventDefault();
     const user = this.state;
     const url = 'https://teachers-placement-backend.herokuapp.com/api/login';
@@ -34,20 +48,33 @@ class SignIn extends Component {
         localStorage.setItem('token', token);
         // this.setState();
         const getToken = localStorage.getItem('token');
-        (getToken && getToken.length !== 0) ? this.props.history.push('/dashboard') : this.props.history.push('/signin');
-      })
-      .catch((error) => console.log(error));
+        if(getToken && getToken.length !== 0){
+          this.setState({ message: "succesfully logged in" });
+          this.showModal()
+         
+        }else{
+          this.setState({ message: "wrong username/password combination" });
+          this.showModal()
+        }
+      }).catch(err=>{
+        this.setState({ message: "wrong username/password combination" });
+        this.showModal()
+  })
   }
 
   render() {
     return (
       <main className="main main-bg">
-        <Navbar home='Home' signup='Sign Up' /> <br /><br />
+        <Navbar  /> 
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <p>{this.state.message}</p>
+        
+        </Modal>
         <h3 className="form-header text-center">Welcome, Sign In</h3>
         <form className="form text-center" onSubmit={this.handleSubmit}>
           <div className="form-group input-icon">
             <input className="form-control input-signin" type="text" name="loginKey" value={this.state.loginKey} onChange={this.handleChange} placeholder="Username/Email/Telephone"/>
-            <div className="icon-input"><i class="fas fa-user" aria-hidden="true"></i></div>
+            <div className="icon-input"><i className="fas fa-user" aria-hidden="true"></i></div>
           </div> <br />
           <div className="form-group input-icon">
             <input className="form-control input-signin" type="password" name="password" value={this.state.password} onChange={this.handleChange} placeholder="Password"/>
