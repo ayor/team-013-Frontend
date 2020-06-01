a1d4c9b97d8d8933f6a16075542627ceda66de3a
Ifere Okibe <jioblck@gmail.com>
2020-06-01 09:10:27 +0100

    Updated App.js and removed tester.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/homepage/Home';
 import SignUp from './components/auth/SignUp';
 import SignIn from './components/auth/SignIn';
-import Tester from './components/auth/tester';
 import Dashboard from './components/dashboard/Dashboard';
 import Test from './components/dashboard/test';
 
@@ -13,7 +12,6 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/tester" component={Tester} />
           <Route path="/signup" component={SignUp} />
           <Route path="/signin" component={SignIn} />
           <Route path="/test" component={Test} />
