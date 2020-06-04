05154e919f19111a7c5c753ab32252681155633b
ayor <39896394+ayor@users.noreply.github.com>
2020-06-04 19:04:12 +0100

    fixed dashboard errors

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Navbar from '../layout/Navbar';
 import Footer from '../footer/Footer';
+import Icon from '../Icon'
 
 
 const token = localStorage.getItem('token');
@@ -12,11 +13,26 @@ const authAxios = axios.create({
   }
 });
 
+const instituteInfo = [
+  { name: 'school', iconType: 'university' },
+  { name: 'courseOfStudy', iconType: 'book-reader' },
+  { name: 'levelOfEducation', iconType: 'graduation-cap' },
+  { name: 'gpa', iconType: 'award' },
+  { name: 'yearOfExperience', iconType: 'star-half-alt' }];
+
+const contactInfo = [
+  { name: 'address', iconType: 'home' },
+  { name: 'phone', iconType: 'phone' },
+  { name: 'country', iconType: 'globe' },
+  { name: 'state', iconType: 'map-marker-alt' },
+  { name: 'username', iconType: 'user' }]
+
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      teacherData: ''
+      teacherData: '',
+      loggedIn: false
     };
   }
 
@@ -27,7 +43,10 @@ class Dashboard extends Component {
         .get(url)
         .then((res) => {
           const teacherData = res.data.data;
-          this.setState({ teacherData });
+          this.setState({
+            teacherData,
+            loggedIn: true
+          });
         })
         .catch((error) => console.log(error));
     } else {
@@ -41,99 +60,125 @@ class Dashboard extends Component {
 
     return (
       <div>
-        {getData.map((data) => (
-          <main className="main dashboard" key={data._id}>
-            <Navbar signout="Log Out" userName={data.firstName} takeTest="Take a Test" /> <br />
-            <br />
-            <br />
-            <br />
-            <div className="flex-wrap">
-              <div className="flex-item">
-                <div className="profile-info">
-                  <div className="profile-info-item">
-                    <div
-                      className="profile-item"
-                      style={{
-                        borderRadius: '50%',
-                        overflow: 'hidden'
-                      }}
-                    >
-                      <img src={`https://teachers-placement-backend.herokuapp.com/${data.image}`} alt="profile picture"/>
-                    </div>{' '}
-                  </div>
-                  <div
-                    className="profile-info-item"
-                    style={{
-                      width: '70%',
-                      textAlign: 'right'
-                    }}
-                  >
-                    <div className="profile-item">
-                      <div>
-                        <h1>{`${data.firstName} ${data.lastName}`}</h1>
-                      </div>
-                      <hr />
+        {getData.map((data) => {
+          let educationalBackground = instituteInfo.map(({ name, iconType }) =>
+            (<div><Icon classType={`fas fa-${iconType}`} iconColor={'#ffd700'} />   {data[name]}</div>));
+
+          let userContact = contactInfo.map(({name,iconType}) => ((<div><Icon classType={`fas fa-${iconType}`} iconColor={'#ffd700'} />   {data[name]}</div>)));
+          return (
+            <main className="main dashboard" key={data._id}>
+              <Navbar signout="Log Out" userName={data.firstName} takeTest="Take a Test" isLoggedIn={this.state.loggedIn} /> <br />
+              <br />
+
+              <div className="flex-wrap">
+                <div className="flex-item">
+                  <div className="profile-info">
+                    <div className="profile-info-item">
                       <div
-                        className="user-rating-info"
+                        className="profile-item"
                         style={{
-                          margin: '1em 0',
-                          display: 'flex',
-                          justifyContent: 'space-between',
-                          textAlign: 'center'
+                          borderRadius: '50%',
+                          overflow: 'hidden'
                         }}
                       >
+                        <img src={`https://teachers-placement-backend.herokuapp.com/${data.image}`} alt="profile picture" />
+                      </div>{' '}
+                    </div>
+                    <div
+                      className="profile-info-item"
+                      style={{
+                        width: '70%',
+                        textAlign: 'right'
+                      }}
+                    >
+                      <div className="profile-item">
                         <div>
-                          <h6>EMAIL</h6>
-                          <p>{data.email}</p>
+                          <h1>{`${data.firstName} ${data.lastName}`}</h1>
                         </div>
-                        <div>
-                          <h6>ROLE</h6>
-                          <p>{data.role}</p>
-                        </div>
+                        <hr />
+                        <div
+                          className="user-rating-info"
+                          style={{
+                            margin: '1em 0',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            textAlign: 'center'
+                          }}
+                        >
+                          <div>
+                            <h6>EMAIL</h6>
+                            <p>{data.email}</p>
+                          </div>
+                          <div>
+                            <h6>ROLE</h6>
+                            <p>{data.role}</p>
+                          </div>
 
-                        <div>
-                          <h6>STATUS</h6>
-                          <p style={{ color: '#228B22' }}>Passed</p>
-                        </div>
-                        <div>
-                          <h6>RATING</h6>
-                          <div style={{ display: 'flex', color: '#DAA520' }}>
-                            <i className="fas fa-star" aria-hidden="true"></i>
-                            <i className="fas fa-star" aria-hidden="true"></i>
-                            <i className="fas fa-star" aria-hidden="true"></i>
-                            <i className="fas fa-star" aria-hidden="true"></i>
-                            <i className="fas fa-star" aria-hidden="true"></i>
+                          <div>
+                            <h6>STATUS</h6>
+                            <p style={{ color: '#228B22' }}>Passed</p>
+                          </div>
+                          <div>
+                            <h6>RATING</h6>
+                            <div style={{ display: 'flex', color: '#DAA520' }}>
+                              <i className="fas fa-star" aria-hidden="true"></i>
+                              <i className="fas fa-star" aria-hidden="true"></i>
+                              <i className="fas fa-star" aria-hidden="true"></i>
+                              <i className="fas fa-star" aria-hidden="true"></i>
+                              <i className="fas fa-star" aria-hidden="true"></i>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="wrap-flex">
-                <div className="data-item">
-                  <ul className="right">
-                    <li>Institution Attended</li>
-                    <li>Level Of Education</li>
-                    <li>Department</li>
-                    <li>GPA</li>
-                    <li>Years Of Experience</li>
-                  </ul>
+                <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                  <div className='education'>
+                    <div>
+                      <h3>Educational Background</h3>
+                      <div id='institution'>
+                        {educationalBackground}
+                      </div>
+                    </div>
+                  </div>
+                  <div className='education'>
+                    <div>
+                      <h3>Contact</h3>
+                      <div id='institution'>
+                        {userContact}
+                      </div>
+
+                    </div>
+
+
+                    {/* <div className="data-item">
+                    <ul className="right">
+                      <li>Institution Attended</li>
+                      <li>Level Of Education</li>
+                      <li>Department</li>
+                      <li>GPA</li>
+                      <li>Years Of Experience</li>
+                    </ul>
+                  </div>
+                  <div className="data-item">
+                    <ul>
+                      <li>{data.school}</li>
+                      <li>{data.levelOfEducation}</li>
+                      <li>{data.courseOfStudy}</li>
+                      <li>{data.gpa}</li>
+                      <li>{data.yearOfExperience}</li>
+                    </ul>
+                  </div> */}
+                  </div>
+
                 </div>
-                <div className="data-item">
-                  <ul>
-                    <li>{data.school}</li>
-                    <li>{data.levelOfEducation}</li>
-                    <li>{data.courseOfStudy}</li>
-                    <li>{data.gpa}</li>
-                    <li>{data.yearOfExperience}</li>
-                  </ul>
-                </div>
+
               </div>
-            </div>
-            <Footer />
-          </main>
-        ))}
+              <Footer />
+            </main>
+          )
+        })}
       </div>
     );
   }
