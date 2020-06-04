ad291434bf1968b83e9286a270f7d1ac9e792250
ayor <39896394+ayor@users.noreply.github.com>
2020-06-04 19:11:17 +0100

    removed error codes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'alertifyjs/build/alertify.min.js';
+import 'alertifyjs/build/css/alertify.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
